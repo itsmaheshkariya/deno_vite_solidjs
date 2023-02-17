d388860b55333ea5960e975cd303b4d842e61fe3
youtube <youtube@mahesh-2.local>
2023-02-17 13:38:42 +0530

    init

---
+++ vite.config.mjs
@@ -0,0 +1,12 @@
+import { defineConfig } from 'npm:vite'
+import solidPlugin from 'npm:vite-plugin-solid';
+
+export default defineConfig({
+  plugins: [solidPlugin()],
+  server: {
+    port: 3000,
+  },
+  build: {
+    target: 'esnext',
+  },
+});
